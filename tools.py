3dd33647f2544278927059c754acd816caf0a953
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-03-31 16:50:05 +0800

    index4.py index3.py index_hsu.py update

--- tools.py
+++ tools.py
@@ -1,10 +1,11 @@
 import requests
 import csv
 from io import StringIO
-#API_KEY = "edc6617a-ae5a-4820-8ef9-c0038e709bd9" KOCHUNG'S API KEY
+import password
+
 
 class Site(object):
-    def __init__(self, name, county, aqi):
+    def __init__(self,name,county,aqi):
         super().__init__()
         self.site_name = name
         self.county = county
@@ -13,33 +14,41 @@ class Site(object):
         except:
             self.aqi = 999
 
-class Sort():
-    pass
-
     def __repr__(self):
-        return f"監測站點：{self.site_name}，所在縣市：{self.county}，aqi={self.aqi}"
+        return f"站點:{self.site_name},城市:{self.county},aqi:{self.aqi}"
 
-class Taiwan_AQI():
-    # API_KEY = "**edc6617a*ae5a*4820*8ef9*c0038e709bd9**" 建立password.py
-    @classmethod
-    def download_aqi(cls) -> list:
 
-        response=requests.get(f'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key={cls.API_KEY}&sort=ImportDate%20desc&format=CSV')
+class Taiwan_AQI(list):    
+    def __init__(self):
+        super().__init__()        
+        response=requests.get(f'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key={password.API_KEY}&limit=1000&sort=ImportDate desc&format=CSV')
 
-        if response.ok:
-            print(response.text)
-            #file = open('./aqi.csv', mode='w', encoding='utf-8')
-            #file.write(response.text)
-            #file.close()
+        if response.ok:            
             file = StringIO(response.text,newline='')
             csvReader = csv.reader(file)
-            next(csvReader)
-            dataList = []
+            next(csvReader)            
             for item in csvReader:
-                site = Site(item[0],item[1],item[2])
-                dataList.append(site)
-            return dataList
-            
+                if item[2] !=  999:
+                    site = Site(item[0],item[1],item[2])
+                    self.append(site)
+                        
         else:
             raise Exception("下載失敗")
     
+    def get_bad(self,n=3):
+        '''
+        @param n,最差的數量
+        取出AQI最差的list
+        list內的元素是Site的實體
+        '''
+        sorted_aqi = sorted(self,key=lambda site:site.aqi)
+        return sorted_aqi[-n:]
+    
+    def get_better(self,n=3):
+        '''
+        @param n,最好的數量
+        取出AQI最好的list
+        list內的元素是Site的實體
+        '''
+        sorted_aqi = sorted(self,key=lambda site:site.aqi)
+        return sorted_aqi[:n]
